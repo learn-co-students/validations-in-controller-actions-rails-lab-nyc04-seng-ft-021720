4faf48f55cd4abd0a91f7ae9534f4cb6178e196f
96StanleyLee <96stanleylee@gmail.com>
2020-03-18 09:35:50 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates_inclusion_of :category, :in => %w(Fiction Non-Fiction)
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
 end
